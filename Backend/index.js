b6b84ea20b90ba2054fcaefc771b00ec60769fa4
Deepak kumar <deep98.r@gmail.com>
2024-08-12 09:53:54 +0530

    render

--- Backend/index.js
+++ Backend/index.js
@@ -21,7 +21,7 @@ app.use(cors({
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+// app.use(session({ secret: process.env.SESSION_SECRET, resave: true, saveUninitialized: true }));
 app.use(session({ 
     secret: process.env.SESSION_SECRET,
      resave: false,
